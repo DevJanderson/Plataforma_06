f927abb5df8cb9595b3437b82780d57c583676cb
JanderLira <devjanderlira@gmail.com>
2022-12-27 17:37:58 -0400

    upload app meu linktree

---
+++ applinks/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApplinksConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'applinks'
--- core/urls.py
+++ core/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('hello.urls'))
+    path('hello/', include('hello.urls')),
+    path('', include('applinks.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

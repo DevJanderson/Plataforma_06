078978cad8430b7b0020e92244985b8daa07a394
JanderLira <devjanderlira@gmail.com>
2023-01-17 13:17:44 -0400

    ocultando chamada do banco de dados

--- core/settings.py
+++ core/settings.py
@@ -78,16 +78,16 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'db.lbunxhzgduitviucgjwp.supabase.co',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '3rIEEZTroIQ5v6Ia',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'HOST': 'db.lbunxhzgduitviucgjwp.supabase.co',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': '3rIEEZTroIQ5v6Ia',
+#         'PORT': '5432',
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
